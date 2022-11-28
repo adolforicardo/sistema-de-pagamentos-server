77eab0c2d7c197dff7f4155e2108bf123d2deebd
Adolfo Ricardo <adolforicardo5@gmail.com>
2022-11-28 22:33:47 +0200

    Created CodigoDeDiscagem table

---
+++ prisma/migrations/20221128203318_created_codigo_de_discagem_table/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE `codigo_de_discagem` (
+    `id` VARCHAR(191) NOT NULL,
+    `pais` VARCHAR(191) NOT NULL,
+    `codigo` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
